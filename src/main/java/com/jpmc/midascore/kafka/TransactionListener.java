1219bb1e2a3a78722bfdf92828ac20d1ac6041d4
Prachit Pandit <20scs047.prachit.16@gmail.com>
2025-08-29 21:00:37 +0530

    Finished Task 1 & 2

---
+++ src/main/java/com/jpmc/midascore/kafka/TransactionListener.java
@@ -0,0 +1,17 @@
+package com.jpmc.midascore.kafka;
+
+import com.jpmc.midascore.foundation.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TransactionListener {
+    private static final Logger LOG = LoggerFactory.getLogger(TransactionListener.class);
+
+    @KafkaListener(topics = "${general.kafka-topic}", groupId = "midas-group")
+    public void listen(Transaction transaction) {
+        LOG.info("Received transaction: {}", transaction);
+    }
+}
